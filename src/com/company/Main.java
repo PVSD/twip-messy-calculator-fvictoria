cd0550e44af91e1bc0be38e1fb4b3ec037dd623f
fvictoria <felipevictoria630@yahoo.com>
2018-09-12 20:07:32 -0400

    hi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,4 @@
 package com.company;
-import java.lang.Math.*;
 import java.util.*;
 public class Main {
 
@@ -16,7 +15,7 @@ public class Main {
         if (operation.equals("add")) {
             double resultOne = Math.sqrt(newFirstNumber+newSecondNumber);
             System.out.println("The sum of " +newFirstNumber + " and " +newSecondNumber+" is " +resultOne +" are you satisfied?");
-            String satisfaction = kbReader.nextLine();
+            String s2 = kbReader.nextLine();
             System.out.println("Can you identify what math class method I ran the real answer through?");
             String mClass = kbReader.nextLine();
             if (mClass.equals("sqrt")) {
@@ -25,15 +24,35 @@ public class Main {
         }
         if (operation.equals("subtract")) {
             double resultTwo = Math.pow(newFirstNumber,newSecondNumber);
-            System.out.println(resultTwo);
+            System.out.println("The difference between "+ newFirstNumber+ " and " +newSecondNumber + " is "+resultTwo+ " are you satisfied?");
+            String s3 = kbReader.nextLine();
+            System.out.println("Can you identify what math class method I ran the real answer through?");
+            String mClass2 = kbReader.nextLine();
+            if (mClass2.equals("power")) {
+                System.out.println("Correct"); }
+
         }
         if (operation.equals("multiply")) {
             double resultThree = newFirstNumber + newSecondNumber;
-            System.out.println(resultThree);
+            System.out.println("The product of "+ newFirstNumber+" and "+ newSecondNumber+" is "+resultThree+" are you satisfied?");
+            String s4 = kbReader.nextLine();
+            System.out.println("Can you identify what math class method I ran the real answer through?");
+            String mClass3 = kbReader.nextLine();
+            if (mClass3.equals("add")) {
+                System.out.println("Correct");
+            }
+
         }
         if (operation.equals("divide")) {
-            double resultFour = newFirstNumber*newSecondNumber;
-            System.out.println(resultFour);
+            double resultFour = newFirstNumber * newSecondNumber;
+            System.out.println("The quotient of "+ newFirstNumber+" and "+newSecondNumber+" is "+resultFour+" are you satisfied?");
+            String s5 = kbReader.nextLine();
+            System.out.println("Can you identify what math class method I ran the real answer through?");
+            String mClass4 = kbReader.nextLine();
+            if (mClass4.equals("multiply")) {
+                System.out.println("Correct");
+            }
+
         }
 
 
